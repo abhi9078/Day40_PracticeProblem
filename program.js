8310357052458c29b2ac50c80812490105a763c7
Abhilash <abhilashmeher1234@gmail.com>
2022-04-23 22:19:59 +0530

    Added program file for check Prime Number problem

--- program.js
+++ program.js
@@ -1,26 +1,15 @@
-var num=prompt("Please enter a number in between 0-6: ")
+var num =prompt("Please enter a number to check for prime number: ");
 
-if(num==0){
-    console.log("SUNDAY");
+var count=0;
+for (var i=1;i<=num;i++){
+    if (num%i==0){
+        count=count+1;
+    }
 }
-else if (num==1){
-    console.log("MONDAY");
-}
-else if (num==2){
-    console.log("TUESDAY");
-}
-else if (num==3){
-    console.log("WEDNESSDAY");
-}
-else if (num==4){
-    console.log("THURSEDAY");
-}
-else if (num==5){
-    console.log("FRIDAY");
-}
-else if (num==6){
-    console.log("SATURDAY");
+
+if (count==2){
+    console.log("Entered number is Prime Number");
 }
 else{
-    console.log("Please enter number in between 0 to 6 ");
+    console.log("Enter number is not a prime number")
 }
