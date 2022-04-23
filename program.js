9a4924c60407c0c0187093402da2e3f8059fc9fe
Abhilash <abhilashmeher1234@gmail.com>
2022-04-23 22:39:20 +0530

    Added program file for Factorial of a number

--- program.js
+++ program.js
@@ -1,15 +1,9 @@
-var frange =prompt("Please enter first range");
-var lrange=prompt("Please enter last range")
+var num= prompt("Enter a number to find factorial: ");
 
-console.log("All prime number with in that range is:");
-for (i=frange;i<=lrange;i++){
-    var count=0;
-    for (var j=1;j<=i;j++){
-        if(i%j==0){
-            count++;
-        }
-    }
-    if(count==2){
-        console.log(i);
-    }
+var fact=1
+for (var i=1;i<=num;i++){
+    fact=fact*i;
+
 }
+
+console.log("Factorial of the number is: ",fact);
