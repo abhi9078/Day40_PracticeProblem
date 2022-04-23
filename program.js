d418731f7f8f78be3a33346b0b04bddc8db1bd34
Abhilash <abhilashmeher1234@gmail.com>
2022-04-23 13:37:18 +0530

    Added program file for Harmonic Number

--- program.js
+++ program.js
@@ -1,7 +1,9 @@
-var range=prompt("Enter range to find power of two: ");
-var sum=1
-for (var i=1; i<=range; i++){
-    sum=sum*2;
+var range=prompt("Enter range to find Harmonic number: ");
+
+var sum=0;
+
+for (var i=1;i<=range;i++){
+    sum=sum+(1/i);
 }
 
-console.log("Power of two table upto the range is: ",sum);
+console.log("Harmonic number upto that range is: ",sum);
