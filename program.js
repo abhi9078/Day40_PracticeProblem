c0021eb6c992942ce241ad74e355b7ee72fa455d
Abhilash <abhilashmeher1234@gmail.com>
2022-04-23 13:22:49 +0530

    Added program file for Converting single digit to word

--- program.js
+++ program.js
@@ -1,8 +1,35 @@
-var year = window.prompt("Enter a year to check leapyear or not: ");
+var num=prompt("Please enter a single digit nuber: ")
 
-if ((year%400==0) || (year%100!=0 && year % 4 == 0)){
-    console.log("entered year is leap year: ",year);
+if(num==0){
+    console.log("ZERO");
+}
+else if (num==1){
+    console.log("ONE");
+}
+else if (num==2){
+    console.log("TWO");
+}
+else if (num==3){
+    console.log("THREE");
+}
+else if (num==4){
+    console.log("FOUR");
+}
+else if (num==5){
+    console.log("FIVE");
+}
+else if (num==6){
+    console.log("SIX");
+}
+else if (num==7){
+    console.log("SEVEN");
+}
+else if (num==8){
+    console.log("EIGHT");
+}
+else if (num==9){
+    console.log("NINE");
 }
 else{
-    console.log("Entered year is Not a Leap year")
+    console.log("Please enter a single digit valid number");
 }
