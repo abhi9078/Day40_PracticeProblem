2dc3eb3a6704863630c1b123cb1ba9c35cbc0896
Abhilash <abhilashmeher1234@gmail.com>
2022-04-23 23:06:00 +0530

    Added program file for Palindrom Number

--- program.js
+++ program.js
@@ -1,9 +1,22 @@
-var num= prompt("Enter a number to find factorial: ");
 
-var fact=1
-for (var i=1;i<=num;i++){
-    fact=fact*i;
+function palindrom(){
+    var num=prompt("Please enter a number to check palindrom or not");
+    var temp=num;
+    var rev=0;
+    var div=0;
+    while(num>0){
+        div=num%10;
+        rev=(rev*10)+div;
+        num=parseInt(num/10);
+    }
+    if(temp==rev){
+        console.log("Entered number is palindrom number ");
+    }
+    else{
+        console.log("Enter number is not a palindrom number ");
+    }
 
 }
 
-console.log("Factorial of the number is: ",fact);
+palindrom();
+
